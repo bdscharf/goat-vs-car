996257eccdab6b648e1b9c03879fd714437f530f
Benjamin Scharf <benjamin.d.scharf@gmail.com>
2019-06-02 19:52:56 -0500

    Add more for game so far.

--- components/Door.js
+++ components/Door.js
@@ -3,54 +3,21 @@ import React, { Component } from 'react';
 class Door extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      doorState: 'closed'
-    }
   }
 
   render() {
+    var doorImg = (<img src='static/images/closed.png'/>);
+    if (this.props.doorNum == this.props.gameState.firstChoice) {
+      doorImg = (<img src='static/images/selected.png'/>);
+    }
+
     return (
       <div className='DoorBox' onClick={this.props.action}>
-        <img src='static/images/closed.png'/>
+        {doorImg}
       </div>
     );
   }
 }
 
 module.exports = Door;
-
-// class Door extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.selectDoor = this.selectDoor.bind(this);
-//   }
-
-//   selectDoor(val) {
-//     console.log('a door has been selected with value: ' + val);
-//     this.props.updateProps({
-//       value: val
-//     });
-//   }
-// }
-
-
-// function createDoorImg(doorType) {
-//   switch(doorType) {
-//     case 'closed':
-//       return (<img src='../static/images/closed.png'/>);
-//     case 'goat':
-//       return (<img src='../static/images/goat.png'/>);
-//     case 'car':
-//       return (<img src='../static/images/car.png'/>);
-//     default:
-//       console.log('[ERROR]: bad input passed into door function.');
-//   }
-// }
-
-// function createDoor(doorType, val) {
-//   return (
-//     <div className='DoorBox' key={val} value={val} onClick={() => this.selectDoor(val)}>
-//       {createDoorImg(doorType)}
-//     </div>);
-// }
 
--- components/Game.js
+++ components/Game.js
@@ -1,30 +1,83 @@
 import React, { Component } from 'react';
 import Door from './Door';
 
+function generateDoors(n) {
+  let doors = new Array(n);
+  for (let i = 0; i < doors.length; i++) {
+    doors[i] = i+1;
+  }
+  return doors;
+}
+
+function getInstructions(gameState) {
+  let text = '';
+  switch(gameState) {
+    case 'firstChoice':
+      return (<p>Choose a door!</p>);
+    case 'stayOrSwitch':
+      return (
+        <button type="button">Stay</button>
+      );
+    default:
+      return (<br />);
+  }
+  return text;
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    // Select car and goat doors
+    let doors = generateDoors(this.props.doorCount); let sample = [];
+    sample.push(doors.splice(Math.random() * doors.length, 1));
+    sample.push(doors.splice(Math.random() * doors.length, 1));
+    // Set initial state
     this.state = {
-      selectedDoor: null
+      firstChoice: null,
+      carDoor: sample[0],
+      goatDoor: sample[1]
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(val) {
-    console.log('Door ' + val + ' clicked.');
-    // console.log('Previous state: ' + JSON.stringify(this.state));
-    // this.setState({
-    //   selectedDoor: childData
-    // });
-    // console.log('New state: ' + JSON.stringify(this.state));
+    switch(this.props.gameStep) {
+      case 'firstChoice': // prompt user to stay or switch
+        console.log('Choice made for door ' + val);
+        this.props.updateProps({
+          gameStep: 'stayOrSwitch'
+        });
+        this.setState({
+          firstChoice: val
+        });
+        break;
+      case 'stayOrSwitch': // time to reveal!
+        this.props.updateProps({
+          gameStep: 'reveal'
+        });
+        break;
+      case 'reveal':
+        console.log('time to reveal');
+        break;
+      default:
+        return 'something went wrong :(';
+    }
+    console.log('State is now: ');
+    console.log(JSON.stringify(this.state));
   }
 
   render() {
+    let doors = generateDoors(this.props.doorCount);
     return (
       <div className='GameBox'>
-        <Door action={() => this.handleClick(1)} />
-        <Door action={() => this.handleClick(2)} />
-        <Door action={() => this.handleClick(3)} />
+        {doors.map((ele) => {
+          return (<Door key={ele} doorNum={ele} gameState={this.state} action={() => this.handleClick(ele)} />);
+        })}
+        {getInstructions(this.props.gameStep)}
       </div>
     );
   }
